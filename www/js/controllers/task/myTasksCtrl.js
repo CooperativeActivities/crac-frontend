672a66ea1a10e33f434c9fcb6c8e94e9dfa745a0
GTN-FabioPernegger <fpernegger@gtn-solutions.com>
2016-11-07 15:53:10 +0100

    little change in myTasks

--- www/js/controllers/task/myTasksCtrl.js
+++ www/js/controllers/task/myTasksCtrl.js
@@ -1,10 +1,15 @@
 /**
  * Created by P41332 on 25.10.2016.
  */
-cracApp.controller('myTasksCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+cracApp.controller('myTasksCtrl', ['$scope', '$stateParams','$routeParams','TaskDataService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-  function ($scope, $stateParams) {
+function ($scope, $stateParams, TaskDataService) {
 
+  TaskDataService.getMyTasks().then(function(res) {
+    console.log(res.data);
+  }, function(error) {
+    console.log('An error occurred!', error);
+  });
 
-  }])
+}])
