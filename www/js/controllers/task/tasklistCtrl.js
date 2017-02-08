8fcd03606d5e2666bcb3fbaee8f9571bd7f2b2b4
Nathanael Huber <nathanael.huber@student.uibk.ac.at>
2017-02-08 12:26:57 +0100

    Accordion Tasklist

--- www/js/controllers/task/tasklistCtrl.js
+++ www/js/controllers/task/tasklistCtrl.js
@@ -7,6 +7,7 @@ cracApp.controller('tasklistCtrl', function ($rootScope, $state, $scope, $http, 
   $scope.loadSingleTask = function(taskId){
     $state.go('tabsController.task', { id:taskId });
   }
+
   $scope.doRefresh = function(){
     $q.all(
       TaskDataService.getMatchingTasks(3).then(function(res){
@@ -23,6 +24,32 @@ cracApp.controller('tasklistCtrl', function ($rootScope, $state, $scope, $http, 
 
   $scope.doRefresh();
 
+  /*----------------------------------------------------------------------------------------------------------------- */
+  $scope.groups = [];
+  for (var i=0; i<10; i++) {
+    $scope.groups[i] = {
+      name: i,
+      items: []
+    };
+    for (var j=0; j<3; j++) {
+      $scope.groups[i].items.push(i + '-' + j);
+    }
+  }
+
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
 
 })
 
