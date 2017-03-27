04f6393114e502f2de29c5c15af86815e20d6610
Moritz Dafert <md@x-net.at>
2017-03-27 19:16:02 +0200

    typo showing team in task detail

--- www/js/services/user_service.js
+++ www/js/services/user_service.js
@@ -14,18 +14,7 @@ cracApp.factory('UserDataService', ["Helpers", function(Helpers){
     return ajax("user/" + id, "get");
   }
 
-  /*
-   * Given a user in the following structure
-     {
-         "name":"test",
-         "password": "test",
-         "role":"USER",
-         "firstName":"TestHans",
-         "lastName":"TestName",
-         "phone":"234",
-         "email":"asd@asd"
-         }
-         */
+  // create new user
   srv.createUser = function(user){
     return ajax("user", "post", { payload: user });
   }
@@ -35,33 +24,12 @@ cracApp.factory('UserDataService', ["Helpers", function(Helpers){
     return ajax("user/" + id, "delete");
   }
 
-  /*
-   * Updates the user with the specified id and new user data
-     {
-         "name":"test",
-         "password": "test",
-         "role":"USER",
-         "firstName":"TestHans",
-         "lastName":"TestName",
-         "phone":"234",
-         "email":"asd@asd"
-      }
-      */
+  // Updates the user with the specified id and new user data
   srv.updateUserById = function(id, newUserData){
     return ajax("user/" + id, "put", { payload: newUserData });
   }
-  /*
-   * Updates the user which is currently logged in and new user data
-   {
-       "name":"test",
-       "password": "test",
-       "role":"USER",
-       "firstName":"TestHans",
-       "lastName":"TestName",
-       "phone":"234",
-       "email":"asd@asd"
-    }
-    */
+
+  // Updates the user which is currently logged in and new user data
   srv.updateCurrentUser = function(newUserData){
     return ajax("user", "put", { payload: newUserData });
   }
@@ -70,6 +38,7 @@ cracApp.factory('UserDataService', ["Helpers", function(Helpers){
   srv.getCurrentUser = function(){
     return ajax("user/all", "get");
   }
+
   //Returns the competences of the currently logged in user, wrapped in the relationship-object
   srv.getCompRelationships = function(){
     return ajax("user/competence", "get");
@@ -98,15 +67,20 @@ cracApp.factory('UserDataService', ["Helpers", function(Helpers){
   srv.updateCompetence = function(id,likeValue,proficiencyValue){
     return ajax("user/competence/" + id + "/adjust/" + likeValue + "/" + proficiencyValue, "get")
   }
-  //Returns all notifications, which target the logged in user
-  srv.getNotification = function(){
-    return ajax("notification", "get");
-  }
   //Show all competences, that are and not yet connected available to a user
   srv.getAllAvailableCompetences = function(){
     return ajax("user/competence/available", "get");
   }
 
+  //Returns all notifications, which target the logged in user
+  srv.getNotification = function(){
+    return ajax("notification", "get");
+  }
+
+  //Returns all notifications in the system
+  srv.getNotificationAll = function(){
+    return ajax("notification/admin", "get");
+  }
 
   return srv
 }])
