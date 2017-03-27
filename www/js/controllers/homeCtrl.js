5a02cac0bc71dd23d0d8da02e734c302fc6150c4
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-03-27 21:02:16 +0200

    update root controllers for new error structure

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -1,27 +1,29 @@
 /**
  * Created by P41332 on 25.10.2016.
  */
-cracApp.controller('homeCtrl', ['$scope', '$stateParams', 'TaskDataService','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+cracApp.controller('homeCtrl', ['$scope', '$stateParams', 'TaskDataService', '$ionicPopup', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-  function ($scope, $stateParams, TaskDataService, $state) {
-
-      TaskDataService.getMatchingTasks().then(function(res){
-        $scope.tasks = res.object;
-      })
-
+  function ($scope, $stateParams, TaskDataService, $ionicPopup, $state) {
+    TaskDataService.getMatchingTasks().then(function(res){
+      $scope.tasks = res.object;
+    }, function(error){
+      console.warn("Matching tasks could not be retrieved", error);
+    });
 
     $scope.follow = function(id){
       TaskDataService.changeTaskState(id,'follow').then(function(data) {
         console.log(data);
       }, function(error) {
-        console.log('An error occurred!', error);
+        $ionicPopup.alert({
+          title: "Aufgabe kann nicht gefolgt werden",
+          template: error.message,
+          okType: 'button-positive button-outline'
+        });
       });
     };
 
     $scope.loadSingleTask = function(taskId){
       $state.go('tabsController.task', { id:taskId });
     }
-
-
-  }])
+  }]);
--- www/js/controllers/myCrAcMenuCtrl.js
+++ www/js/controllers/myCrAcMenuCtrl.js
@@ -2,10 +2,10 @@
  * Created by P41332 on 25.10.2016.
  */
 
-cracApp.controller('myCrAcMenuCtrl', ['$scope','$rootScope', '$stateParams','UserDataService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+cracApp.controller('myCrAcMenuCtrl', ['$scope','$rootScope', '$stateParams','UserDataService','$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-  function ($scope,$rootScope, $stateParams, UserDataService) {
+  function ($scope,$rootScope, $stateParams, UserDataService, $ionicPopup) {
     //console.log("globals", $rootScope.globals);
     $rootScope.$watch(['globals.hasOwnProperty'], function() {
       if ($rootScope.globals.hasOwnProperty("currentUser")) {
@@ -15,14 +15,19 @@ cracApp.controller('myCrAcMenuCtrl', ['$scope','$rootScope', '$stateParams','Use
           }, function(error){
             // this error happens when the user has no competences assigned
             // just catching this error cause i don't want it to clutter the console
-          })
+            //@TODO Should this really be an error?
+          });
           var user = res.object;
           $scope.user = user;
           $rootScope.globals.userInfo = user;
           //console.log("user", $scope.user);
 
         }, function (error) {
-          console.log('An error occurred!', error);
+          $ionicPopup.alert({
+            title: "Benutzerinformation konnte nicht geladen werden",
+            template: error.message,
+            okType: 'button-positive button-outline'
+          });
         });
       }
     })
