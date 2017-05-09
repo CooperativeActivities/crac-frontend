6681fe66f85c2001da5c2a0467aaccbeed00f14d
silvester <silvester.triebnig@aon.at>
2017-05-09 11:18:46 +0200

    fixed routing bug for task edit aswell

--- www/js/routes.js
+++ www/js/routes.js
@@ -242,6 +242,14 @@ angular.module('app.routes', ['ionicUIRouter'])
         url: '/newtask/map',
         params: {id:null, address:null},
         views: {
+          'tab1': {
+            templateUrl: 'templates/map.html',
+            controller: 'MapController'
+          },
+          'tab2': {
+            templateUrl: 'templates/map.html',
+            controller: 'MapController'
+          },
           'tab3': {
             templateUrl: 'templates/map.html',
             controller: 'MapController'
