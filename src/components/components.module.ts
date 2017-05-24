142f14ddef8befee942ca6f1db2b997cb29d6a8e
Moritz Dafert <md@x-net.at>
2017-05-24 12:46:18 +0200

    collapsible card component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { TaskPreviewComponent } from './task-preview/task-preview';
+import { CollapsibleCardComponent } from './collapsible-card/collapsible-card';
 import { IonicModule } from "ionic-angular"
 
 @NgModule({
   declarations: [
     TaskPreviewComponent,
+    CollapsibleCardComponent,
   ],
   imports: [
     IonicModule
   ],
   exports: [
-    TaskPreviewComponent
+    TaskPreviewComponent,
+    CollapsibleCardComponent,
   ]
 })
 export class ComponentsModule {}
