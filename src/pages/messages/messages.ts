4c6dc74aab8c2301dad5c025386ddffe42dc5ab5
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-06-01 22:46:46 +0200

    migrate messages page

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import * as _ from 'lodash';
+
+import { UserDataService } from "../../services/user_service";
 
 @IonicPage({
   name: "messages",
@@ -7,52 +10,75 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-messages',
   templateUrl: 'messages.html',
+  providers: [ UserDataService ],
 })
 export class MessagesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  notifications: Array<any> = [];
+
+  constructor(public navCtrl: NavController, public userDataService: UserDataService) {
+    this.onReload();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MessagesPage');
   }
 
-}
-  /*
-cracApp.controller('messagingCtrl', ['$scope', '$stateParams','UserDataService', "$q",
-  function ($scope, $stateParams, UserDataService, $q) {
-    $scope.reload = function(){
-      UserDataService.getNotification().then(function(res){
-        var notifications = res.object
-        var promises = []
-        notifications.forEach(function(notification){
-          if(notification.name = "Friend Request"){
-            promises.push(UserDataService.getUserById(notification.senderId).then(function(res){
-              notification.user = res.object
-            }))
-          }
-        })
-        return $q.all(promises).then(function(){
-          $scope.notifications = notifications
-        })
-      }, function(error) {
-        console.log('An error occurred!', error);
-      }).then(function(){
-        $scope.$broadcast('scroll.refreshComplete');
-      });
-    }
+  accept(notification){
+    let self = this;
 
-    $scope.accept = function (notification){
-      console.log(notification)
-      UserDataService.acceptNotification(notification.notificationId).then(function(){
-        $scope.reload()
-      })
-    }
-    $scope.decline = function (notification){
-      UserDataService.declineNotification(notification.notificationId).then(function(){
-        $scope.reload()
-      })
-    }
-    $scope.reload()
-  }])
-   */
+    console.log(notification);
+    self.userDataService.acceptNotification(notification.notificationId).then(function(){
+      let index = _.findIndex(self.notifications, {id: notification.notificationId});
+      self.notifications.splice(index, 1)[0];
+    }, function(error) {
+      this.toast.create({
+        message: "Nachrichten konnte nicht zugestimmt werden: " + error.message,
+        duration: 3000,
+        position: 'top'
+      }).present();
+    });
+  }
+
+  decline(notification){
+    let self = this;
+
+    self.userDataService.declineNotification(notification.notificationId).then(function(){
+      let index = _.findIndex(self.notifications, {id: notification.notificationId});
+      self.notifications.splice(index, 1)[0];
+    }, function(error) {
+      this.toast.create({
+        message: "Nachrichten konnte nicht abgelehnt werden: " + error.message,
+        duration: 3000,
+        position: 'top'
+      }).present();
+    });
+  }
+  onReload() {
+    let self = this;
+
+    self.userDataService.getNotification().then(function(res){
+      let notifications = res.object;
+      if(notifications.length === 0) {
+        return [new Promise((resolve) => {resolve(false)})];
+      }
+      let promises = [];
+      notifications.forEach(function(notification){
+        if(notification.name = "Friend Request"){
+          promises.push(self.userDataService.getUserById(notification.senderId).then(function(res){
+            notification.user = res.object
+          }))
+        }
+      });
+      return Promise.all(promises).then(function(){
+        self.notifications = notifications
+      });
+    }, function(error) {
+      this.toast.create({
+        message: "Nachrichten konnte nicht geladen werden: " + error.message,
+        duration: 3000,
+        position: 'top'
+      }).present();
+    });
+  }
+}
