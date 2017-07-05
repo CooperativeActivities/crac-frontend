19ecb9ec1051ffb3fde225f7e99c1910d5e090b9
Tobias Riepl <tobiasriepl@gmx.at>
2017-07-05 18:06:34 +0200

    prepare evaluation-detail

--- src/pages/evaluation-detail/evaluation-detail.ts
+++ src/pages/evaluation-detail/evaluation-detail.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TaskDataService } from '../../services/task_service';
+import _ from "lodash"
 
-
-@IonicPage()
+@IonicPage({
+  name: "evaluation-detail",
+  segment: "evaluation-detail",
+})
 @Component({
   selector: 'page-evaluation-detail',
   templateUrl: 'evaluation-detail.html',
+  providers: [ TaskDataService ]
 })
 export class EvaluationDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public taskDataService: TaskDataService) {
   }
 
   ionViewDidLoad() {
