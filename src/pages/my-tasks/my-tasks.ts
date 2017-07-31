bc67c7bb79dbaf0749b1af3d307e949551135242
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-07-31 15:35:35 +0200

    add filters to my task list (RM 3029)

--- src/pages/my-tasks/my-tasks.ts
+++ src/pages/my-tasks/my-tasks.ts
@@ -14,13 +14,21 @@ import {UserDataService} from "../../services/user_service";
 })
 export class MyTasksPage {
 
+  allTasks: any;
+  filters: any;
   participatingTasks : any[];
   followingTasks : any[];
   leadingTasks: any[];
   userHasPermissions: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public taskDataService: TaskDataService, public userDataService: UserDataService) { }
+              public taskDataService: TaskDataService, public userDataService: UserDataService) {
+    this.filters = {
+      participating: 'started',
+      following: 'started',
+      leading: 'started'
+    };
+  }
 
   ionViewDidEnter() {
     this.doRefresh();
@@ -30,6 +38,45 @@ export class MyTasksPage {
     this.navCtrl.push('task-edit');
   }
 
+  /*getOpenParticipation(t) {
+    if(t.participationDetails === undefined || t.participationDetails.length === 0) return false;
+    return !t.participationDetails[0].complete;
+  }*/
+
+  filterTasks(type, filter) {
+    switch(filter) {
+      case 'open':
+        return this.allTasks[type].filter(this.getPublishedTasks);
+      case 'started':
+        return this.allTasks[type].filter(this.getStartedTasks);
+      case 'closed':
+        return this.allTasks[type].filter(this.getClosedTasks);
+      default:
+        return this.allTasks[type];
+    }
+  }
+  filterParticipatingTasks(e) {
+    this.participatingTasks = this.filterTasks('participating', e.value);
+  }
+  filterFollowingTasks(e) {
+    this.followingTasks = this.filterTasks('following', e.value);
+  }
+  filterLeadingTasks(e) {
+    this.leadingTasks = this.filterTasks('leading', e.value);
+  }
+
+  getPublishedTasks(t) {
+    return t.taskState === 'PUBLISHED';
+  }
+
+  getStartedTasks(t) {
+    return t.taskState === 'STARTED';
+  }
+
+  getClosedTasks(t) {
+    return t.taskState === 'COMPLETED';
+  }
+
   getOpenTasks(t) {
     return t.taskState !== 'COMPLETED';
   }
@@ -37,9 +84,14 @@ export class MyTasksPage {
   async doRefresh (refresher=null) {
     await Promise.all([
       this.taskDataService.getMyTasks().then((res) => {
-        this.participatingTasks = _.orderBy(res.meta.participating, [ "startTime" ], [ "asc" ]).filter(this.getOpenTasks);
-        this.followingTasks = _.orderBy(res.meta.following, [ "startTime" ], [ "asc" ]).filter(this.getOpenTasks);
-        this.leadingTasks = _.orderBy(res.meta.leading, [ "startTime" ], [ "asc" ]).filter(this.getOpenTasks);
+        this.allTasks = {
+          participating: res.meta.participating,
+          following: res.meta.following,
+          leading: res.meta.leading
+        };
+        this.participatingTasks = _.orderBy(res.meta.participating, [ "startTime" ], [ "asc" ]).filter(this.getStartedTasks);
+        this.followingTasks = _.orderBy(res.meta.following, [ "startTime" ], [ "asc" ]).filter(this.getStartedTasks);
+        this.leadingTasks = _.orderBy(res.meta.leading, [ "startTime" ], [ "asc" ]).filter(this.getStartedTasks);
       }, (error) => {
         console.warn("Matching tasks could not be retrieved", error)
       }),
