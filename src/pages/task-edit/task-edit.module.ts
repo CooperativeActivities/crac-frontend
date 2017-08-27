3d464586a1f0409837effb7b647584e6d0aa7736
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-08-28 01:54:15 +0200

    add in min-value validator, validate number of volunteers (incl shifts)

--- src/pages/task-edit/task-edit.module.ts
+++ src/pages/task-edit/task-edit.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TaskEditPage } from './task-edit';
 import { ComponentsModule } from "../../components/components.module"
+import { MinValueValidator } from "../../directives/min-value";
 
 @NgModule({
   declarations: [
     TaskEditPage,
+    MinValueValidator
   ],
   imports: [
     IonicPageModule.forChild(TaskEditPage),
