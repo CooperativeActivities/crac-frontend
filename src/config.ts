2350654410416a7cbe0c55d3997be6426bea2ae8
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2017-08-28 18:24:43 +0200

    add phone number validator (edit profile) - issue with touched prop causes it to validate early (ionic bug 12102)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 (<any>window).crac_config = {
   // example: "https://my.url.at/path/"
-  SERVER: "REPLACE_SERVER",
+  SERVER: "https://core.crac.at/crac-core/",
   mapzenKey: "mapzen-FZZdZ5c",
 }
--- src/pages/profile-edit/profile-edit.module.ts
+++ src/pages/profile-edit/profile-edit.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfileEditPage } from './profile-edit';
 import { ComponentsModule } from "../../components/components.module"
+import {PhoneNumberValidator} from "../../directives/phone-number";
 
 @NgModule({
   declarations: [
     ProfileEditPage,
+    PhoneNumberValidator
   ],
   imports: [
     IonicPageModule.forChild(ProfileEditPage),
